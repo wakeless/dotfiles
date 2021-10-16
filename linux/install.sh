5bed3e3b47433a57e9735ce48c83f8c50ef638fc
Michael Gall <michael@wakeless.net>
2021-10-17 00:28:11 +1100

    Install as well

--- linux/install.sh
+++ linux/install.sh
@@ -6,7 +6,5 @@ fi
 
 sudo add-apt-repository ppa:x4121/ripgrep
 sudo apt-get update
-sudo apt-get install ripgrep
-sudo apt-get install bc
-sudo apt-get install hub
+sudo apt-get install -Y ripgrep bc hub
 

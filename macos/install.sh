daf697c41dbf4f73f9b8a9963b85e64161b69ea5
Michael Gall <michael@wakeless.net>
2021-10-28 01:36:40 +1100

    Don't exit 1

--- macos/install.sh
+++ macos/install.sh
@@ -3,7 +3,7 @@
 # command line interface to it that we can use to just install everything, so
 # yeah, let's do that.
 if [ `uname` != "Darwin" ]; then
-  exit 1
+  exit 0
 fi
 
 echo "› sudo softwareupdate -i -a"

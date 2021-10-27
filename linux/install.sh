27fbe2564bf5ddbdb83c020717014b814653f7e3
Michael Gall <michael@wakeless.net>
2021-10-28 01:00:40 +1100

    More fixes. Slowest feedback loop ever

--- linux/install.sh
+++ linux/install.sh
@@ -4,8 +4,8 @@ if [ `uname` != "Linux" ]; then
     echo "Run on Linux (not on Mac OS X)"; exit 1
 fi
 
-sudo apt-get install software-properties-common
+sudo apt-get install -y --no-install-recommends software-properties-common
 sudo add-apt-repository ppa:x4121/ripgrep
 sudo apt-get update
-sudo apt-get install -y --no-install-recommendsy ripgrep bc hub
+sudo apt-get install -y --no-install-recommends ripgrep bc hub
 
